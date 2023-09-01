f66c9117b0f4af21314ff7ef6df2d2a1848cb4ce
leen <leenodeh287@gmail.com>
2023-09-01 15:47:28 +0300

    added taskTwo

--- 03_Day_Setting_Up/tasks-MySolution/src/App.js
+++ 03_Day_Setting_Up/tasks-MySolution/src/App.js
@@ -1,9 +1,11 @@
 import TaskOne from "./Compponent/TaskOne/TaskOne";
+import TaskTwo from "./Compponent/TaskTwo/TaskTwo";
 
 function App() {
   return (
     <div className="App">
       <TaskOne />
+      <TaskTwo/>
     </div>
   );
 }
